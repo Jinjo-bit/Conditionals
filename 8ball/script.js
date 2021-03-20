e218ba69d502e3c3708c4bcb8f429eb02585bd86
Jinjo-bit <71643734+Jinjo-bit@users.noreply.github.com>
2021-03-20 07:24:55 -0700

    Add files via upload

---
+++ 8ball/script.js
@@ -0,0 +1,36 @@
+var userName = ''
+'Quez' ? console.log('Hello, Quez!') :
+console.log('Hello!');
+var userQuestion = 'Can i do a flip?'
+console.log(`can i do a flip?: ${userQuestion}`);
+const randomNumber = Math.floor(Math.random() * 8);
+let = ''
+switch (randomNumber) {
+  case 0:
+    eightBall = 'YES'
+    break;
+  case 1:
+    eightBall = 'yeah'
+    break;
+  case 2:
+    eightBall = 'ask again'
+    break;
+  case 3:
+   eightBall = 'Cannot predict now'
+    break;
+  case 4:
+    eightBall = 'dont rely on it'
+    break;
+  case 5:
+    eightBall = 'my buddy says no'
+    break;
+  case 6:
+    eightBall = 'most unlikely'
+    break;
+  case 7:
+  eightBall ='Theres no way!'
+}
+console.log(`The eight ball answered:
+ ${eightBall}`);
+var eightBall = ''
+console.log(eightBall);
